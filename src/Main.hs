ed9b31cea6c71205184b44595884ec7304f24c29
masaedw <masa.edw@gmail.com>
2013-05-16 05:54:10 +0900

    Implement main loop

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
 module Main (main) where
 
+import Data.List
+import Evolution
+import Evolution.Imports
+
 main :: IO ()
-main = undefined
+main = do
+  let world = initWorld 20 10
+  gen <- getStdGen
+  void $ runStateT (loop world) gen
+  where
+    loop :: World -> StateT StdGen IO World
+    loop world = do
+      nw <- step world
+      liftIO . putStr $ showWorld nw
+      liftIO . putStrLn $ "--------------"
+      line <- liftIO getLine
+      if ("q" `isPrefixOf` line)
+        then return nw
+        else loop nw
